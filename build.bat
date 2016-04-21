3b15f907306dcdd0f39e840135b8b9629c6bb3c9
shepc <chris.shepherd@ao.com>
2016-04-21 09:24:43 +0100

    Adding sangria to build.bat

--- build.bat
+++ build.bat
@@ -22,6 +22,8 @@ call %NuGet% restore src\Sidecar\packages.config -OutputDirectory %cd%\packages 
 call %NuGet% restore tests\Sidecar.Tests\packages.config -OutputDirectory %cd%\packages -NonInteractive
 call %NuGet% restore src\Spritz\packages.config -OutputDirectory %cd%\packages -NonInteractive
 call %NuGet% restore tests\Spritz.Tests\packages.config -OutputDirectory %cd%\packages -NonInteractive
+call %NuGet% restore src\Sangria\packages.config -OutputDirectory %cd%\packages -NonInteractive
+call %NuGet% restore tests\Sangria.Tests\packages.config -OutputDirectory %cd%\packages -NonInteractive
 
 REM Build
 msbuild Screwdriver.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
@@ -32,6 +34,7 @@ call %nuget% install NUnit.Runners -Version 2.6.4 -OutputDirectory packages
 packages\NUnit.Runners.2.6.4\tools\nunit-console.exe /config:%config% /framework:net-4.5 tests\Screwdriver.Tests\bin\%config%\Screwdriver.Tests.dll
 packages\NUnit.Runners.2.6.4\tools\nunit-console.exe /config:%config% /framework:net-4.5 tests\Sidecar.Tests\bin\%config%\Sidecar.Tests.dll
 packages\NUnit.Runners.2.6.4\tools\nunit-console.exe /config:%config% /framework:net-4.5 tests\Spritz.Tests\bin\%config%\Spritz.Tests.dll
+packages\NUnit.Runners.2.6.4\tools\nunit-console.exe /config:%config% /framework:net-4.5 tests\Sangria.Tests\bin\%config%\Sangria.Tests.dll
 if not "%errorlevel%"=="0" goto failure
 
 REM Package
@@ -41,6 +44,7 @@ mkdir Build\lib\net40
 call %nuget% pack "src\Screwdriver.nuspec" -IncludeReferencedProjects -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
 call %nuget% pack "src\Sidecar.nuspec" -IncludeReferencedProjects -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
 call %nuget% pack "src\Spritz.nuspec" -IncludeReferencedProjects -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+call %nuget% pack "src\Sangria.nuspec" -IncludeReferencedProjects -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
 
 :success
 exit 0
